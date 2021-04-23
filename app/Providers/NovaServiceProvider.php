be69527881331097131b36f07117709ca7ce5414
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-23 19:47:48 +0800

    Add reproducing code for laravel/nova-issues#3364
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -17,6 +17,10 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
+
+        Nova::booted(function () {
+            app()->setLocale('nl');
+        });
     }
 
     /**
